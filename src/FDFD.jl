110516a2d18fd4ac492e9931f25eed2540e62ecc
kabume <kabukabu1@outlook.com>
2021-05-15 19:55:57 +0800

    change FDFD.jl

--- src/FDFD.jl
+++ src/FDFD.jl
@@ -2,6 +2,12 @@ module FDFD
 
 using Arpack, SparseArrays
 
+include("geometry.jl")
+export Geometry
+
+include("modes.jl")
+export Mode, HilbertSpace, get_field
+
 include("utils.jl")
 
 include("solve_fdm.jl")
