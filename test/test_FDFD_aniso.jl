87e21e7fdb82e8bd101cb05d0b95fe3e2c27ef7c
kabume <kabukabu1@outlook.com>
2021-05-18 23:01:30 +0800

    change test field

--- test/test_FDFD_aniso.jl
+++ test/test_FDFD_aniso.jl
@@ -2,7 +2,7 @@ using Peacock
 using PyPlot
 #using Peacock.FDFD
 
-Polarisation = TE
+Polarisation = TM
 N_eig = 4
 eps0 = 1; mu0 = 1
 # build geometry
@@ -10,7 +10,7 @@ eps0 = 1; mu0 = 1
 eps1 = [15 0 0; 0 15 0; 0 0 15]
 mu1 = [14 12.4im 0; -12.4im 14 0; 0 0 1]
 #eps1 = [14 -12.4im 0; 12.4im 14 0; 0 0 15]
-if Polarisation == TE
+if Polarisation == TE #TODO add to another function
     eps1 = inv(eps1)
 else
     mu1 = inv(mu1)
